e2646eb86c113f2f5d4044875265186fc2125b6e
Joeri Jackers <joeri.jackers@kbc.be>
2018-09-16 13:42:14 +0200

    first effort to integrate typescript and semantic ui

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// import semantic ui stuff
+import SuiVue from 'semantic-ui-vue';
+import 'semantic-ui-css/semantic.min.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(SuiVue);
 
 new Vue({
   router,
